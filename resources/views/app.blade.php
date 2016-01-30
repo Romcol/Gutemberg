e149de704ead351a9d47d6706d51ee2bdf5c79dc
Colombat Romain <romain.colombat@insa-rennes.fr>
2016-01-30 20:07:46 +0100

    Initial Laravel Devlopment - Static Home Page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Gutemberg</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+	@yield('css_includes')
+</head>
+
+<body>
+	@yield('page_content')
+	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,62 @@
+@extends('app')
+
+@section('css_includes')
+<link rel="stylesheet" href="<?php echo asset('css/cover.css')?>" type="text/css"> 
+@stop
+
+@section('page_content')
+<div class="site-wrapper">
+
+  <div class="site-wrapper-inner">
+
+    <div class="cover-container">
+
+      <div class="masthead clearfix">
+        <div class="inner">
+          <h3 class="masthead-brand">Gutemberg</h3>
+          <nav>
+            <ul class="nav masthead-nav">
+              <li class="active"><a href="#">Accueil</a></li>
+              <li><a href="#">S'inscrire</a></li>
+              <li><a href="#">Se connecter</a></li>
+              <li><a href="#">Aide</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div class="inner cover">
+        <!--<h1 class="cover-heading">Cover your page.</h1>
+        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-default">Learn more</a>
+        </p>-->
+        <p class="lead">Bienvenue sur Gutemberg
+        <form class="form-inline">
+		  <div class="form-group">
+		    <input type="text" class="form-control" id="search_input" placeholder="Rechercher">
+		  </div>
+		  <div class="form-group">
+		      <select name="type" class="form-control">
+					<option value="articles">Articles</option>
+					<option value="revues">Revues de presse</option>
+					<option value="journeaux">Journaux</option>
+  				</select>
+		  </div>
+		  <button type="submit" class="btn btn-default">Recherche</button>
+		</form>
+        </p>
+      </div>
+
+      <div class="mastfoot">
+        <div class="inner">
+          <p>Footer</p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+@stop
