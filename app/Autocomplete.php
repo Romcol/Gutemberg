df933143ba56009d1d49f65ee53ad0889b90288a
Bouchet Alexandre <alexandre.bouchet@insa-rennes.fr>
2016-05-17 14:27:07 +0200

    Fix autocomplete data issue

--- app/Autocomplete.php
+++ app/Autocomplete.php
@@ -8,6 +8,7 @@ use Fadion\Bouncy\BouncyTrait;
 class Autocomplete extends Moloquent
 {
   use BouncyTrait;
+  public $timestamps  = false;
   protected $collection = 'AutocompleteData';
   protected $indexName = 'autocomplete';
   protected $typeName = 'data';
