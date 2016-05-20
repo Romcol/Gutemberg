2efbe106cba83db182e6219d48b8590e996b3967
Colombat Romain <romain.colombat@insa-rennes.fr>
2016-05-20 18:54:18 +0200

    Utilisation CDN bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
     <title>Gutemberg</title>
 
     <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="<?php echo asset('css/bootstrap.min.css')?>" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
     <!-- jQuery css -->
     <link rel="stylesheet" href="<?php echo asset('css/jquery-ui.css')?>" type="text/css">
@@ -107,7 +107,7 @@
     <script src="<?= asset('/js/jquery-ui.js') ?>"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="<?= asset('/js/bootstrap.min.js') ?>"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
     @yield('scripts')
 
